87a60af49b03be135d472cf8666f280c0a276170
amandainnis <amanda.innis@gmail.com>
2016-02-28 19:09:14 -0500

    p5

---
+++ js/untitled2script.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	var x = document.getElementById("divshape3");
+	x.style.backgroundColor = "#939315";
+	x.addEventListener('click',function(event){
+		x.style.backgroundColor = "#159193";
+	})
+}
