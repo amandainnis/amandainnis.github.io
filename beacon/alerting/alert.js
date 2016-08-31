895468a36f98dde0e9b37570076a7d25783bdca0
Amanda Innis <amanda.innis@gmail.com>
2016-08-31 12:52:54 -0400

    numbers changing, minRow +1px padding

--- beacon/alerting/alert.js
+++ beacon/alerting/alert.js
@@ -31,8 +31,52 @@ $(document).ready(function(){
 		$('#systmSummary').text(openTotalSummaryBoxTotals[6]);
 		$('#mrktSummary').text(openTotalSummaryBoxTotals[7]);
 
+		///////// MINIS ////////
+
+		$('#riskSummaryMy').text(Math.round(Math.random() * openTotalSummaryBoxTotals[0]));
+		$('#riskSummaryDsk').text(Math.round(Math.random() * openTotalSummaryBoxTotals[0]));
+		$('#riskSummaryCov').text(Math.round(Math.random() * openTotalSummaryBoxTotals[0]));
+		$('#riskSummaryVip').text(Math.round(Math.random() * openTotalSummaryBoxTotals[0]));
+
+		$('#tradeSummaryMy').text(Math.round(Math.random() * openTotalSummaryBoxTotals[1]));
+		$('#tradeSummaryDsk').text(Math.round(Math.random() * openTotalSummaryBoxTotals[1]));
+		$('#tradeSummaryCov').text(Math.round(Math.random() * openTotalSummaryBoxTotals[1]));
+		$('#tradeSummaryVip').text(Math.round(Math.random() * openTotalSummaryBoxTotals[1]));
+
+		$('#stratSummaryMy').text(Math.round(Math.random() * openTotalSummaryBoxTotals[2]));
+		$('#stratSummaryDsk').text(Math.round(Math.random() * openTotalSummaryBoxTotals[2]));
+		$('#stratSummaryCov').text(Math.round(Math.random() * openTotalSummaryBoxTotals[2]));
+		$('#stratSummaryVip').text(Math.round(Math.random() * openTotalSummaryBoxTotals[2]));
+
+		$('#salesSummaryMy').text(Math.round(Math.random() * openTotalSummaryBoxTotals[3]));
+		$('#salesSummaryDsk').text(Math.round(Math.random() * openTotalSummaryBoxTotals[3]));
+		$('#salesSummaryCov').text(Math.round(Math.random() * openTotalSummaryBoxTotals[3]));
+		$('#salesSummaryVip').text(Math.round(Math.random() * openTotalSummaryBoxTotals[3]));
+
+		$('#perfSummaryMy').text(Math.round(Math.random() * openTotalSummaryBoxTotals[4]));
+		$('#perfSummaryDsk').text(Math.round(Math.random() * openTotalSummaryBoxTotals[4]));
+		$('#perfSummaryCov').text(Math.round(Math.random() * openTotalSummaryBoxTotals[4]));
+		$('#perfSummaryVip').text(Math.round(Math.random() * openTotalSummaryBoxTotals[4]));
+
+		$('#prTrdSummaryMy').text(Math.round(Math.random() * openTotalSummaryBoxTotals[5]));
+		$('#prTrdSummaryDsk').text(Math.round(Math.random() * openTotalSummaryBoxTotals[5]));
+		$('#prTrdSummaryCov').text(Math.round(Math.random() * openTotalSummaryBoxTotals[5]));
+		$('#prTrdSummaryVip').text(Math.round(Math.random() * openTotalSummaryBoxTotals[5]));
+
+		$('#systmSummaryMy').text(Math.round(Math.random() * openTotalSummaryBoxTotals[6]));
+		$('#systmSummaryDsk').text(Math.round(Math.random() * openTotalSummaryBoxTotals[6]));
+		$('#systmSummaryCov').text(Math.round(Math.random() * openTotalSummaryBoxTotals[6]));
+		$('#systmSummaryVip').text(Math.round(Math.random() * openTotalSummaryBoxTotals[6]));
+
+		$('#mrktSummaryMy').text(Math.round(Math.random() * openTotalSummaryBoxTotals[7]));
+		$('#mrktSummaryDsk').text(Math.round(Math.random() * openTotalSummaryBoxTotals[7]));
+		$('#mrktSummaryCov').text(Math.round(Math.random() * openTotalSummaryBoxTotals[7]));
+		$('#mrktSummaryVip').text(Math.round(Math.random() * openTotalSummaryBoxTotals[7]));
+
 	});
 	// end of total
+
+	//////////////////////////////  OUTAGE ////////////////////////////////////
 
 	$('#outage').click(function(){
 		$('.summary .numbers').removeClass('selectedNumbers');
@@ -46,8 +90,52 @@ $(document).ready(function(){
 		$('#systmSummary').text(outageSummaryBoxTotals[6]);
 		$('#mrktSummary').text(outageSummaryBoxTotals[7]);
 
+		///////// MINIS ////////
+
+		$('#riskSummaryMy').text(Math.round(Math.random() * outageSummaryBoxTotals[0]));
+		$('#riskSummaryDsk').text(Math.round(Math.random() * outageSummaryBoxTotals[0]));
+		$('#riskSummaryCov').text(Math.round(Math.random() * outageSummaryBoxTotals[0]));
+		$('#riskSummaryVip').text(Math.round(Math.random() * outageSummaryBoxTotals[0]));
+
+		$('#tradeSummaryMy').text(Math.round(Math.random() * outageSummaryBoxTotals[1]));
+		$('#tradeSummaryDsk').text(Math.round(Math.random() * outageSummaryBoxTotals[1]));
+		$('#tradeSummaryCov').text(Math.round(Math.random() * outageSummaryBoxTotals[1]));
+		$('#tradeSummaryVip').text(Math.round(Math.random() * outageSummaryBoxTotals[1]));
+
+		$('#stratSummaryMy').text(Math.round(Math.random() * outageSummaryBoxTotals[2]));
+		$('#stratSummaryDsk').text(Math.round(Math.random() * outageSummaryBoxTotals[2]));
+		$('#stratSummaryCov').text(Math.round(Math.random() * outageSummaryBoxTotals[2]));
+		$('#stratSummaryVip').text(Math.round(Math.random() * outageSummaryBoxTotals[2]));
+
+		$('#salesSummaryMy').text(Math.round(Math.random() * outageSummaryBoxTotals[3]));
+		$('#salesSummaryDsk').text(Math.round(Math.random() * outageSummaryBoxTotals[3]));
+		$('#salesSummaryCov').text(Math.round(Math.random() * outageSummaryBoxTotals[3]));
+		$('#salesSummaryVip').text(Math.round(Math.random() * outageSummaryBoxTotals[3]));
+
+		$('#perfSummaryMy').text(Math.round(Math.random() * outageSummaryBoxTotals[4]));
+		$('#perfSummaryDsk').text(Math.round(Math.random() * outageSummaryBoxTotals[4]));
+		$('#perfSummaryCov').text(Math.round(Math.random() * outageSummaryBoxTotals[4]));
+		$('#perfSummaryVip').text(Math.round(Math.random() * outageSummaryBoxTotals[4]));
+
+		$('#prTrdSummaryMy').text(Math.round(Math.random() * outageSummaryBoxTotals[5]));
+		$('#prTrdSummaryDsk').text(Math.round(Math.random() * outageSummaryBoxTotals[5]));
+		$('#prTrdSummaryCov').text(Math.round(Math.random() * outageSummaryBoxTotals[5]));
+		$('#prTrdSummaryVip').text(Math.round(Math.random() * outageSummaryBoxTotals[5]));
+
+		$('#systmSummaryMy').text(Math.round(Math.random() * outageSummaryBoxTotals[6]));
+		$('#systmSummaryDsk').text(Math.round(Math.random() * outageSummaryBoxTotals[6]));
+		$('#systmSummaryCov').text(Math.round(Math.random() * outageSummaryBoxTotals[6]));
+		$('#systmSummaryVip').text(Math.round(Math.random() * outageSummaryBoxTotals[6]));
+
+		$('#mrktSummaryMy').text(Math.round(Math.random() * outageSummaryBoxTotals[7]));
+		$('#mrktSummaryDsk').text(Math.round(Math.random() * outageSummaryBoxTotals[7]));
+		$('#mrktSummaryCov').text(Math.round(Math.random() * outageSummaryBoxTotals[7]));
+		$('#mrktSummaryVip').text(Math.round(Math.random() * outageSummaryBoxTotals[7]));
+
 	});
 	// end of outage
+
+	//////////////////////////////  CRITICAL ////////////////////////////////////
 
 	$('#critical').click(function(){
 		$('.summary .numbers').removeClass('selectedNumbers');
@@ -61,8 +149,52 @@ $(document).ready(function(){
 		$('#systmSummary').text(criticalSummaryBoxTotals[6]);
 		$('#mrktSummary').text(criticalSummaryBoxTotals[7]);
 
+		///////// MINIS ////////
+
+		$('#riskSummaryMy').text(Math.round(Math.random() * criticalSummaryBoxTotals[0]));
+		$('#riskSummaryDsk').text(Math.round(Math.random() * criticalSummaryBoxTotals[0]));
+		$('#riskSummaryCov').text(Math.round(Math.random() * criticalSummaryBoxTotals[0]));
+		$('#riskSummaryVip').text(Math.round(Math.random() * criticalSummaryBoxTotals[0]));
+
+		$('#tradeSummaryMy').text(Math.round(Math.random() * criticalSummaryBoxTotals[1]));
+		$('#tradeSummaryDsk').text(Math.round(Math.random() * criticalSummaryBoxTotals[1]));
+		$('#tradeSummaryCov').text(Math.round(Math.random() * criticalSummaryBoxTotals[1]));
+		$('#tradeSummaryVip').text(Math.round(Math.random() * criticalSummaryBoxTotals[1]));
+
+		$('#stratSummaryMy').text(Math.round(Math.random() * criticalSummaryBoxTotals[2]));
+		$('#stratSummaryDsk').text(Math.round(Math.random() * criticalSummaryBoxTotals[2]));
+		$('#stratSummaryCov').text(Math.round(Math.random() * criticalSummaryBoxTotals[2]));
+		$('#stratSummaryVip').text(Math.round(Math.random() * criticalSummaryBoxTotals[2]));
+
+		$('#salesSummaryMy').text(Math.round(Math.random() * criticalSummaryBoxTotals[3]));
+		$('#salesSummaryDsk').text(Math.round(Math.random() * criticalSummaryBoxTotals[3]));
+		$('#salesSummaryCov').text(Math.round(Math.random() * criticalSummaryBoxTotals[3]));
+		$('#salesSummaryVip').text(Math.round(Math.random() * criticalSummaryBoxTotals[3]));
+
+		$('#perfSummaryMy').text(Math.round(Math.random() * criticalSummaryBoxTotals[4]));
+		$('#perfSummaryDsk').text(Math.round(Math.random() * criticalSummaryBoxTotals[4]));
+		$('#perfSummaryCov').text(Math.round(Math.random() * criticalSummaryBoxTotals[4]));
+		$('#perfSummaryVip').text(Math.round(Math.random() * criticalSummaryBoxTotals[4]));
+
+		$('#prTrdSummaryMy').text(Math.round(Math.random() * criticalSummaryBoxTotals[5]));
+		$('#prTrdSummaryDsk').text(Math.round(Math.random() * criticalSummaryBoxTotals[5]));
+		$('#prTrdSummaryCov').text(Math.round(Math.random() * criticalSummaryBoxTotals[5]));
+		$('#prTrdSummaryVip').text(Math.round(Math.random() * criticalSummaryBoxTotals[5]));
+
+		$('#systmSummaryMy').text(Math.round(Math.random() * criticalSummaryBoxTotals[6]));
+		$('#systmSummaryDsk').text(Math.round(Math.random() * criticalSummaryBoxTotals[6]));
+		$('#systmSummaryCov').text(Math.round(Math.random() * criticalSummaryBoxTotals[6]));
+		$('#systmSummaryVip').text(Math.round(Math.random() * criticalSummaryBoxTotals[6]));
+
+		$('#mrktSummaryMy').text(Math.round(Math.random() * criticalSummaryBoxTotals[7]));
+		$('#mrktSummaryDsk').text(Math.round(Math.random() * criticalSummaryBoxTotals[7]));
+		$('#mrktSummaryCov').text(Math.round(Math.random() * criticalSummaryBoxTotals[7]));
+		$('#mrktSummaryVip').text(Math.round(Math.random() * criticalSummaryBoxTotals[7]));
+
 	});
 	// end of critical
+
+	//////////////////////////////  URGENT ////////////////////////////////////
 
 	$('#urgent').click(function(){
 		$('.summary .numbers').removeClass('selectedNumbers');
@@ -76,8 +208,52 @@ $(document).ready(function(){
 		$('#systmSummary').text(urgentSummaryBoxTotals[6]);
 		$('#mrktSummary').text(urgentSummaryBoxTotals[7]);
 
+		///////// MINIS ////////
+
+		$('#riskSummaryMy').text(Math.round(Math.random() * urgentSummaryBoxTotals[0]));
+		$('#riskSummaryDsk').text(Math.round(Math.random() * urgentSummaryBoxTotals[0]));
+		$('#riskSummaryCov').text(Math.round(Math.random() * urgentSummaryBoxTotals[0]));
+		$('#riskSummaryVip').text(Math.round(Math.random() * urgentSummaryBoxTotals[0]));
+
+		$('#tradeSummaryMy').text(Math.round(Math.random() * urgentSummaryBoxTotals[1]));
+		$('#tradeSummaryDsk').text(Math.round(Math.random() * urgentSummaryBoxTotals[1]));
+		$('#tradeSummaryCov').text(Math.round(Math.random() * urgentSummaryBoxTotals[1]));
+		$('#tradeSummaryVip').text(Math.round(Math.random() * urgentSummaryBoxTotals[1]));
+
+		$('#stratSummaryMy').text(Math.round(Math.random() * urgentSummaryBoxTotals[2]));
+		$('#stratSummaryDsk').text(Math.round(Math.random() * urgentSummaryBoxTotals[2]));
+		$('#stratSummaryCov').text(Math.round(Math.random() * urgentSummaryBoxTotals[2]));
+		$('#stratSummaryVip').text(Math.round(Math.random() * urgentSummaryBoxTotals[2]));
+
+		$('#salesSummaryMy').text(Math.round(Math.random() * urgentSummaryBoxTotals[3]));
+		$('#salesSummaryDsk').text(Math.round(Math.random() * urgentSummaryBoxTotals[3]));
+		$('#salesSummaryCov').text(Math.round(Math.random() * urgentSummaryBoxTotals[3]));
+		$('#salesSummaryVip').text(Math.round(Math.random() * urgentSummaryBoxTotals[3]));
+
+		$('#perfSummaryMy').text(Math.round(Math.random() * urgentSummaryBoxTotals[4]));
+		$('#perfSummaryDsk').text(Math.round(Math.random() * urgentSummaryBoxTotals[4]));
+		$('#perfSummaryCov').text(Math.round(Math.random() * urgentSummaryBoxTotals[4]));
+		$('#perfSummaryVip').text(Math.round(Math.random() * urgentSummaryBoxTotals[4]));
+
+		$('#prTrdSummaryMy').text(Math.round(Math.random() * urgentSummaryBoxTotals[5]));
+		$('#prTrdSummaryDsk').text(Math.round(Math.random() * urgentSummaryBoxTotals[5]));
+		$('#prTrdSummaryCov').text(Math.round(Math.random() * urgentSummaryBoxTotals[5]));
+		$('#prTrdSummaryVip').text(Math.round(Math.random() * urgentSummaryBoxTotals[5]));
+
+		$('#systmSummaryMy').text(Math.round(Math.random() * urgentSummaryBoxTotals[6]));
+		$('#systmSummaryDsk').text(Math.round(Math.random() * urgentSummaryBoxTotals[6]));
+		$('#systmSummaryCov').text(Math.round(Math.random() * urgentSummaryBoxTotals[6]));
+		$('#systmSummaryVip').text(Math.round(Math.random() * urgentSummaryBoxTotals[6]));
+
+		$('#mrktSummaryMy').text(Math.round(Math.random() * urgentSummaryBoxTotals[7]));
+		$('#mrktSummaryDsk').text(Math.round(Math.random() * urgentSummaryBoxTotals[7]));
+		$('#mrktSummaryCov').text(Math.round(Math.random() * urgentSummaryBoxTotals[7]));
+		$('#mrktSummaryVip').text(Math.round(Math.random() * urgentSummaryBoxTotals[7]));
+
 	});
 	// end of urgent
+
+	//////////////////////////////  COMMENTARY ////////////////////////////////////
 
 	$('#commentary').click(function(){
 		$('.summary .numbers').removeClass('selectedNumbers');
@@ -91,8 +267,52 @@ $(document).ready(function(){
 		$('#systmSummary').text(commentarySummaryBoxTotals[6]);
 		$('#mrktSummary').text(commentarySummaryBoxTotals[7]);
 
+		///////// MINIS ////////
+
+		$('#riskSummaryMy').text(Math.round(Math.random() * commentarySummaryBoxTotals[0]));
+		$('#riskSummaryDsk').text(Math.round(Math.random() * commentarySummaryBoxTotals[0]));
+		$('#riskSummaryCov').text(Math.round(Math.random() * commentarySummaryBoxTotals[0]));
+		$('#riskSummaryVip').text(Math.round(Math.random() * commentarySummaryBoxTotals[0]));
+
+		$('#tradeSummaryMy').text(Math.round(Math.random() * commentarySummaryBoxTotals[1]));
+		$('#tradeSummaryDsk').text(Math.round(Math.random() * commentarySummaryBoxTotals[1]));
+		$('#tradeSummaryCov').text(Math.round(Math.random() * commentarySummaryBoxTotals[1]));
+		$('#tradeSummaryVip').text(Math.round(Math.random() * commentarySummaryBoxTotals[1]));
+
+		$('#stratSummaryMy').text(Math.round(Math.random() * commentarySummaryBoxTotals[2]));
+		$('#stratSummaryDsk').text(Math.round(Math.random() * commentarySummaryBoxTotals[2]));
+		$('#stratSummaryCov').text(Math.round(Math.random() * commentarySummaryBoxTotals[2]));
+		$('#stratSummaryVip').text(Math.round(Math.random() * commentarySummaryBoxTotals[2]));
+
+		$('#salesSummaryMy').text(Math.round(Math.random() * commentarySummaryBoxTotals[3]));
+		$('#salesSummaryDsk').text(Math.round(Math.random() * commentarySummaryBoxTotals[3]));
+		$('#salesSummaryCov').text(Math.round(Math.random() * commentarySummaryBoxTotals[3]));
+		$('#salesSummaryVip').text(Math.round(Math.random() * commentarySummaryBoxTotals[3]));
+
+		$('#perfSummaryMy').text(Math.round(Math.random() * commentarySummaryBoxTotals[4]));
+		$('#perfSummaryDsk').text(Math.round(Math.random() * commentarySummaryBoxTotals[4]));
+		$('#perfSummaryCov').text(Math.round(Math.random() * commentarySummaryBoxTotals[4]));
+		$('#perfSummaryVip').text(Math.round(Math.random() * commentarySummaryBoxTotals[4]));
+
+		$('#prTrdSummaryMy').text(Math.round(Math.random() * commentarySummaryBoxTotals[5]));
+		$('#prTrdSummaryDsk').text(Math.round(Math.random() * commentarySummaryBoxTotals[5]));
+		$('#prTrdSummaryCov').text(Math.round(Math.random() * commentarySummaryBoxTotals[5]));
+		$('#prTrdSummaryVip').text(Math.round(Math.random() * commentarySummaryBoxTotals[5]));
+
+		$('#systmSummaryMy').text(Math.round(Math.random() * commentarySummaryBoxTotals[6]));
+		$('#systmSummaryDsk').text(Math.round(Math.random() * commentarySummaryBoxTotals[6]));
+		$('#systmSummaryCov').text(Math.round(Math.random() * commentarySummaryBoxTotals[6]));
+		$('#systmSummaryVip').text(Math.round(Math.random() * commentarySummaryBoxTotals[6]));
+
+		$('#mrktSummaryMy').text(Math.round(Math.random() * commentarySummaryBoxTotals[7]));
+		$('#mrktSummaryDsk').text(Math.round(Math.random() * commentarySummaryBoxTotals[7]));
+		$('#mrktSummaryCov').text(Math.round(Math.random() * commentarySummaryBoxTotals[7]));
+		$('#mrktSummaryVip').text(Math.round(Math.random() * commentarySummaryBoxTotals[7]));
+
 	});
 	// end of commentary
+
+	//////////////////////////////  PROACT ////////////////////////////////////
 
 	$('#proAct').click(function(){
 		$('.summary .numbers').removeClass('selectedNumbers');
@@ -105,6 +325,48 @@ $(document).ready(function(){
 		$('#prTrdSummary').text(proActSummaryBoxTotals[5]);
 		$('#systmSummary').text(proActSummaryBoxTotals[6]);
 		$('#mrktSummary').text(proActSummaryBoxTotals[7]);
+
+		///////// MINIS ////////
+
+		$('#riskSummaryMy').text(Math.round(Math.random() * proActSummaryBoxTotals[0]));
+		$('#riskSummaryDsk').text(Math.round(Math.random() * proActSummaryBoxTotals[0]));
+		$('#riskSummaryCov').text(Math.round(Math.random() * proActSummaryBoxTotals[0]));
+		$('#riskSummaryVip').text(Math.round(Math.random() * proActSummaryBoxTotals[0]));
+
+		$('#tradeSummaryMy').text(Math.round(Math.random() * proActSummaryBoxTotals[1]));
+		$('#tradeSummaryDsk').text(Math.round(Math.random() * proActSummaryBoxTotals[1]));
+		$('#tradeSummaryCov').text(Math.round(Math.random() * proActSummaryBoxTotals[1]));
+		$('#tradeSummaryVip').text(Math.round(Math.random() * proActSummaryBoxTotals[1]));
+
+		$('#stratSummaryMy').text(Math.round(Math.random() * proActSummaryBoxTotals[2]));
+		$('#stratSummaryDsk').text(Math.round(Math.random() * proActSummaryBoxTotals[2]));
+		$('#stratSummaryCov').text(Math.round(Math.random() * proActSummaryBoxTotals[2]));
+		$('#stratSummaryVip').text(Math.round(Math.random() * proActSummaryBoxTotals[2]));
+
+		$('#salesSummaryMy').text(Math.round(Math.random() * proActSummaryBoxTotals[3]));
+		$('#salesSummaryDsk').text(Math.round(Math.random() * proActSummaryBoxTotals[3]));
+		$('#salesSummaryCov').text(Math.round(Math.random() * proActSummaryBoxTotals[3]));
+		$('#salesSummaryVip').text(Math.round(Math.random() * proActSummaryBoxTotals[3]));
+
+		$('#perfSummaryMy').text(Math.round(Math.random() * proActSummaryBoxTotals[4]));
+		$('#perfSummaryDsk').text(Math.round(Math.random() * proActSummaryBoxTotals[4]));
+		$('#perfSummaryCov').text(Math.round(Math.random() * proActSummaryBoxTotals[4]));
+		$('#perfSummaryVip').text(Math.round(Math.random() * proActSummaryBoxTotals[4]));
+
+		$('#prTrdSummaryMy').text(Math.round(Math.random() * proActSummaryBoxTotals[5]));
+		$('#prTrdSummaryDsk').text(Math.round(Math.random() * proActSummaryBoxTotals[5]));
+		$('#prTrdSummaryCov').text(Math.round(Math.random() * proActSummaryBoxTotals[5]));
+		$('#prTrdSummaryVip').text(Math.round(Math.random() * proActSummaryBoxTotals[5]));
+
+		$('#systmSummaryMy').text(Math.round(Math.random() * proActSummaryBoxTotals[6]));
+		$('#systmSummaryDsk').text(Math.round(Math.random() * proActSummaryBoxTotals[6]));
+		$('#systmSummaryCov').text(Math.round(Math.random() * proActSummaryBoxTotals[6]));
+		$('#systmSummaryVip').text(Math.round(Math.random() * proActSummaryBoxTotals[6]));
+
+		$('#mrktSummaryMy').text(Math.round(Math.random() * proActSummaryBoxTotals[7]));
+		$('#mrktSummaryDsk').text(Math.round(Math.random() * proActSummaryBoxTotals[7]));
+		$('#mrktSummaryCov').text(Math.round(Math.random() * proActSummaryBoxTotals[7]));
+		$('#mrktSummaryVip').text(Math.round(Math.random() * proActSummaryBoxTotals[7]));
 
 	});
 	// end of proAct
