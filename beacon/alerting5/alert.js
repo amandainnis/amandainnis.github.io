2f901acd247f715cf65f0848cc3ccdc9f06063f0
Amanda Innis <amanda.innis@gmail.com>
2016-11-10 15:52:35 -0500

    latest

--- beacon/alerting5/alert.js
+++ beacon/alerting5/alert.js
@@ -74,6 +74,8 @@ $(document).ready(function() {
 
     });
 
+    
+
     var tickerBoxArrayMaster = [
      		{ticker: "AAPL", number: "41", redDot: true},
      		{ticker: "ABEV", number: "73", redDot: false},
@@ -130,8 +132,39 @@ $(document).ready(function() {
         ];
 
 
+var clientBoxArrayMaster = [
+            {ticker: "Alambicx", number: "41", redDot: true},
+            {ticker: "alkdsa", number: "73", redDot: false},
+            {ticker: "allipwst", number: "18", redDot: false},
+            {ticker: "alydarx", number: "38", redDot: false},
+            {ticker: "amcedsa", number: "46", redDot: false},
+            {ticker: "aqrpropx5", number: "51", redDot: false},
+            {ticker: "arrowgrtx", number: "21", redDot: false},
+            {ticker: "balyasnyx", number: "35", redDot: true},
 
+            {ticker: "brockst", number: "96", redDot: false},
+            {ticker: "btigdsa", number: "16", redDot: false},
+            {ticker: "caerusx", number: "14", redDot: true},
+            {ticker: "canteshx", number: "37", redDot: false},
+            {ticker: "cantustp", number: "18", redDot: false},
+            {ticker: "chlmnms", number: "38", redDot: true},
+            {ticker: "deepbltsx", number: "46", redDot: false},
+            {ticker: "deshdma", number: "51", redDot: true},
 
+            {ticker: "everpnt", number: "21", redDot: false},
+            {ticker: "flowbvx6", number: "16", redDot: false},
+            {ticker: "govlanex", number: "35", redDot: false},
+            {ticker: "grahamx", number: "96", redDot: true},
+            {ticker: "hwcmdsa", number: "41", redDot: true},
+            {ticker: "janexno", number: "73", redDot: false},
+            {ticker: "jonedsa", number: "18", redDot: true},
+            {ticker: "RIGTWLO", number: "51", redDot: false}
+       
+        ];
+
+        var clientBoxArray = [];
+
+        clientBoxArray = clientBoxArrayMaster.slice(0,8);
 
      // if (tickerCounter == 0) {
      //        tickerBoxArray = tickerBoxArrayMaster.slice(0,10);
@@ -144,23 +177,33 @@ $(document).ready(function() {
       
     
      
-
+// the functions that make tickers and client boxes
 
 
  function makeTickerBox(name, alertCount, crit) {
        
    		var newTicker = $('<div class="summaryBoxStock"><div class="summaryBoxLeft"><div class="stockBoxTitle">'+ name +'</div><div class="numberSummaryBoxStock">'+ alertCount +'</div></div></div>');
    		if (crit) {
-   			
    			newTicker.append($('<div class="stockBorderCritical"></div>'));
-            
-            // $("stockBorder").css( "background-color", "red" );
+          
    		}
         else {newTicker.append($('<div class="stockBorder"></div>'));}
    		$('#tickerZone').append(newTicker);
    		console.log(newTicker);
    };
 
+ function makeClientBox(name, alertCount, crit) {
+       
+        var newTicker = $('<div class="summaryBox"><div class="summaryBoxLeft"><div class="clientBoxTitle">'+ name +'</div><div class="numberSummaryBoxClient">'+ alertCount +'</div></div></div>');
+        if (crit) {
+            
+            newTicker.append($('<div class="colorMarkerClientCrit"></div>'));
+     
+        }
+        else {newTicker.append($('<div class="colorMarkerClient"></div>'));}
+        $('#clientZone').append(newTicker);
+        console.log(newTicker);
+   };
 
 
 // //the function that yields the critArray
@@ -176,12 +219,19 @@ $(document).ready(function() {
      console.log(critArray);
 
 
+
 // the function that iterates through and displays the tickers
 
 
     function iterateDisplay(arr) {
        for (i = 0; i < arr.length; i++) {
        makeTickerBox(arr[i].ticker, arr[i].number, arr[i].redDot);
+        
+     }};
+
+     function iterateDisplayClient(arr) {
+       for (i = 0; i < arr.length; i++) {
+       makeClientBox(arr[i].ticker, arr[i].number, arr[i].redDot);
         
      }};
 
@@ -254,20 +304,20 @@ $('#az').click();
             tickerBoxArray = tickerBoxArrayMaster.slice(0,10);
             $('.circle').removeClass('circleSelected');
             $('#circle1').addClass('circleSelected');
-            $('#stockChevronLeft').hide();
-            $('#stockChevronRight').show();
+            // $('#stockChevronLeft').hide();
+            // $('#stockChevronRight').show();
         } else if (tickerCounter == 1) {
             tickerBoxArray = tickerBoxArrayMaster.slice(10,20);
             $('.circle').removeClass('circleSelected');
             $('#circle2').addClass('circleSelected');
-            $('#stockChevronLeft').show();
-            $('#stockChevronRight').show();
+            // $('#stockChevronLeft').show();
+            // $('#stockChevronRight').show();
         } else if (tickerCounter == 2) {
             tickerBoxArray = tickerBoxArrayMaster.slice(20,30);
             $('.circle').removeClass('circleSelected');
             $('#circle3').addClass('circleSelected');
-            $('#stockChevronLeft').show();
-            $('#stockChevronRight').hide();
+            // $('#stockChevronLeft').show();
+            // $('#stockChevronRight').hide();
         }
         $('#tickerZone').children().remove();
         iterateDisplay(tickerBoxArray); 
