bfbd24ed96ca14f0ecc963fe5033c6263622aa8c
Amanda Innis <amanda.innis@gmail.com>
2016-12-07 14:09:42 -0500

    lines

--- beacon/crb/crb.js
+++ beacon/crb/crb.js
@@ -1,50 +1,51 @@
 
+function textFlip(sectionID){
+	$('#'+ sectionID + '.buySection').on( "mouseenter", function() {
+		$( '#'+ sectionID + ' .spread1' ).text(.315);
+		$( '#'+ sectionID + ' .spread2' ).text(.361);}).on("mouseleave", function() {
+			$( '#'+ sectionID + ' .spread1' ).text(160.315);
+			$( '#'+ sectionID + ' .spread2' ).text(160.361); 
+		});
 
+}
 
 
 $(document).ready(function(){
+	
 
- 	
-	// $('.buySection').hover(function(){
-	// 	$( this ).fadeOut( 100 );
-	// });
-
-	// $('#buy400.buySection').hover(function(){
-		
-	// 	$( '#buy400 .spread1' ).text(.315);
-	// });
+ 	textFlip('buy400');
+ 	textFlip('buy200');
+ 	textFlip('buy100');
+ 	textFlip('buy50');
+	
 
 	// $('#buy400.buySection').on( "mouseenter", function() {
-	// 	$( '#buy400 .spread1' ).text(.315);}).on("mouseleave", function() {
-	// 		$( '#buy400 .spread1' ).text(160.315); });
+	// 	$( '#buy400 .spread1' ).text(.315);
+	// 	$( '#buy400 .spread2' ).text(.361);}).on("mouseleave", function() {
+	// 		$( '#buy400 .spread1' ).text(160.315);
+	// 		$( '#buy400 .spread2' ).text(160.361); 
+	// 	});
 
-	$('#buy400.buySection').on( "mouseenter", function() {
-		$( '#buy400 .spread1' ).text(.315);
-		$( '#buy400 .spread2' ).text(.361);}).on("mouseleave", function() {
-			$( '#buy400 .spread1' ).text(160.315);
-			$( '#buy400 .spread2' ).text(160.361); 
-		});
+	// $('#buy200.buySection').on( "mouseenter", function() {
+	// 	$( '#buy200 .spread1' ).text(.315);
+	// 	$( '#buy200 .spread2' ).text(.361);}).on("mouseleave", function() {
+	// 		$( '#buy200 .spread1' ).text(160.315);
+	// 		$( '#buy200 .spread2' ).text(160.361); 
+	// 	});
 
-	$('#buy200.buySection').on( "mouseenter", function() {
-		$( '#buy200 .spread1' ).text(.315);
-		$( '#buy200 .spread2' ).text(.361);}).on("mouseleave", function() {
-			$( '#buy200 .spread1' ).text(160.315);
-			$( '#buy200 .spread2' ).text(160.361); 
-		});
+	// $('#buy100.buySection').on( "mouseenter", function() {
+	// 	$( '#buy100 .spread1' ).text(.315);
+	// 	$( '#buy100 .spread2' ).text(.361);}).on("mouseleave", function() {
+	// 		$( '#buy100 .spread1' ).text(160.315);
+	// 		$( '#buy100 .spread2' ).text(160.361); 
+	// 	});
 
-	$('#buy100.buySection').on( "mouseenter", function() {
-		$( '#buy100 .spread1' ).text(.315);
-		$( '#buy100 .spread2' ).text(.361);}).on("mouseleave", function() {
-			$( '#buy100 .spread1' ).text(160.315);
-			$( '#buy100 .spread2' ).text(160.361); 
-		});
-
-	$('#buy50.buySection').on( "mouseenter", function() {
-		$( '#buy50 .spread1' ).text(.315);
-		$( '#buy50 .spread2' ).text(.361);}).on("mouseleave", function() {
-			$( '#buy50 .spread1' ).text(160.315);
-			$( '#buy50 .spread2' ).text(160.361); 
-		});
+	// $('#buy50.buySection').on( "mouseenter", function() {
+	// 	$( '#buy50 .spread1' ).text(.315);
+	// 	$( '#buy50 .spread2' ).text(.361);}).on("mouseleave", function() {
+	// 		$( '#buy50 .spread1' ).text(160.315);
+	// 		$( '#buy50 .spread2' ).text(160.361); 
+	// 	});
 		
 
 
